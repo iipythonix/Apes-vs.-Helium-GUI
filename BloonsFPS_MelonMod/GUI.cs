a0f038e59afb9f6ea7f9e7a6853288ee7cef97fb
iipythonix <79859739+iipythonix@users.noreply.github.com>
2021-04-19 01:29:32 -0400

    Add files via upload

---
+++ BloonsFPS_MelonMod/GUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using MelonLoader;
+
+namespace BloonsFPS_MelonMod
+{
+    public class GUI : MelonMod
+    {
+        public override void OnGUI()
+        {
+            Button(new Rect(0, 0, 250, 100), "Button", GUIStyle.none);
+        }
+
+        private static void Button(Rect rect, string v, GUIStyle none)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BloonsFPS_MelonMod/MelonMain.cs
@@ -0,0 +1,42 @@
+﻿using MelonLoader;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace BloonsFPSMelonMod
+{
+    /// <summary>
+    /// If you need help with your mods, join the Bloons FPS discord server: https://discord.gg/6dMwHkzCyj
+    /// Thanks to Sayan for creating such an Awesome fan game!
+    /// </summary>
+    public class MelonMain : MelonMod
+    {
+        // "modFolder" is the location of where your mods data will be. Ex: BloonsFPS/Mods/MyModsFolder
+        // Assembly.GetExecutingAssembly()  will automatically get your mod's filename. Don't modify this line.
+        internal static string modFolder = $"{Environment.CurrentDirectory}\\Mods\\{Assembly.GetExecutingAssembly().GetName().Name}";
+
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("Mod has finished loading.");
+            MelonLogger.Msg("Please feel free to contribute to my github I take pull requests! https://github.com/iipythonix/Apes-vs.-Helium-GUI");
+        }
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                MelonLogger.Msg("You just pressed T");
+            }
+        }
+
+        
+
+        public override void OnSceneWasLoaded(int buildIndex, string levelName)
+        {
+            if (levelName is null)
+            {
+                throw new ArgumentNullException(nameof(levelName));
+            }
+        }
+    }
+}
