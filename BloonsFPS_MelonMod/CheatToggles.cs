7273cf4545ec534da00506179225636dbe95a60e
Glatrix/Coderbox1 <73367967+Glatrix@users.noreply.github.com>
2021-04-21 11:40:37 -0500

    Create CheatToggles.cs

---
+++ BloonsFPS_MelonMod/CheatToggles.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloonsFPS_MelonMod
+{
+    class CheatToggles
+    {
+        // public static bool 
+        public static bool GUIEnabled = false;
+
+
+        public static bool nobalooncollision = false;
+
+
+    }
+}
