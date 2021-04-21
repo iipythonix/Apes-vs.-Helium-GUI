b27d441fd88a4078641af9557f3537b10a0ea8fa
iipythonix <79859739+iipythonix@users.noreply.github.com>
2021-04-21 13:24:46 -0400

    Update file with WIP GUI

---
+++ BloonsFPS_MelonMod/KeyBinds.cs
@@ -0,0 +1,24 @@
+﻿using BloonsFPSMelonMod;
+using MelonLoader;
+
+namespace BloonsFPS_MelonMod
+{
+    public class KeyBinds : MelonMod
+    {
+
+        public class EnableKeybinds
+        {
+            public static bool enableKeybinds = true;
+        }
+
+        public class ToggleKeybinds
+        {
+            
+        }
+
+        private class EnableKeybindsGetSet
+        {
+            public bool enableKeybinds { get; set; }
+        }
+    }
+}
---
+++ BloonsFPS_MelonMod/WeaponHack.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BloonsFPSMelonMod
+{
+    public class WeaponHack
+    {
+       
+    }
+}
