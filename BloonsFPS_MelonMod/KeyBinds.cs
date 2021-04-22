a44f63c621f6200024217711c5adab164b19db88
iipythonix <79859739+iipythonix@users.noreply.github.com>
2021-04-22 13:15:45 -0400

    GUI actually working now my keyboard thanks me.

---
+++ BloonsFPS_MelonMod/KeyBinds.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BloonsFPS_MelonMod;
+
+namespace BloonsFPS_MelonMod
+{
+    class CheatToggles
+    {
+        public static bool GUIEnabled;
+        public static bool isAlreadyLogged;
+
+        public static bool nobalooncollision;
+    }
+}
