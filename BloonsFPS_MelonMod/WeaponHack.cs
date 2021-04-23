39a955df4de989b8ea13289b518f901d1bc7fd35
iipythonix <79859739+iipythonix@users.noreply.github.com>
2021-04-23 13:46:20 -0400

    Add files via upload

---
+++ BloonsFPS_MelonMod/WeaponHack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MelonLoader;
+using UnityEngine;
+using BloonsFPSMelonMod;
+using BloonsFPS_MelonMod;
+
+namespace BloonsFPSMelonMod
+{
+    public class WeaponHack
+    {
+        //List float
+        public static void CustomUpdate()
+        {
+            _weapon = UnityEngine.Object.FindObjectsOfType<Weapon>().ToList<Weapon>();
+        }
+        public static List<Weapon> _weapon;
+        public int damage;
+        public float attackspeed;
+
+        public static int Value { get; set; }
+
+        //Make the stupid dumb text read
+        public void OnUpdate()
+        {
+            if (CheatToggles.enableWeaponBuff == true)
+            {
+                CheatToggles.GUIWeapon = true;
+
+            }
+        }
+    }
+}
